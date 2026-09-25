b736a0de82d40dba132679c08a5a0696c0d4f9ce
Aryan dilip chavan <aryanwono1@gmail.com>
2026-09-26 02:44:16 +0530

    Time: 3 ms (74.48%), Space: 13.3 MB (37.72%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -11,6 +11,7 @@ public:
         m['M'] = 1000;
         int r=0;
         int prev=0;
+        // in this left is greare than right
         for(int i=s.size()-1;i>=0;i--){
             int curr=m[s[i]];
             if(curr<prev){
